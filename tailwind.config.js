7dfa2c0bedb0988c91ea5519c8e45d66ae197e7d
Aaron Ford-Wright <aaron.fordwright@gmail.com>
2023-02-18 23:49:24 -0500

    Create a React webpage and publish it to GitHub Pages

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        eggshell: '#ecebf3',
+        shade: '#0c120c',
+        agreeable: '#6d7275',
+        crimson: '#9e1313',
+        peace: '#c7d6d5'
+      }
+    }
+  },
+  plugins: []
+};
